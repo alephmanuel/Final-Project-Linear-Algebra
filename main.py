56a7fa8a0feb503a5b6586ca2cd3467503dc686d
alephmanuel <aleph.gonzalez@upr.edu>
2024-05-14 23:41:03 -0400

    finished3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Students Name: Aleph M. Gonzalez Pagan & Efran X. Vera Sonera
-Student Number: 840-20-8993 & ???
+Student Number: 840-20-8993 & 802-20-2308
 Student Course: CCOM4065-LD0 - Linear Algebra
 --------------------------------------------------Final Project: Main File---------------------------------------------------------
 """
